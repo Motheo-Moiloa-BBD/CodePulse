f813833c17995c42b05756b6a5105505940c882d
Motheo Moiloa <motheo@bbd.co.za>
2024-02-16 15:52:45 +0200

    Add category list component and home component

--- app/codepulse/src/app/home/home-routing.module.ts
+++ app/codepulse/src/app/home/home-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryListComponent } from './features/category-list/category-list.component';
+import { HomeComponent } from './features/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'categories',
+    component: CategoryListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HomeRoutingModule { }
+export class HomeRoutingModule {}
