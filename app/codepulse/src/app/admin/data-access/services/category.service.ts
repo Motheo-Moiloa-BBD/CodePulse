4a20ae68032e544f330f4f546f7407a73d6353d2
Motheo Moiloa <motheo@bbd.co.za>
2024-02-20 11:59:57 +0200

    Update category service to use api URL from app configuration service

--- app/codepulse/src/app/admin/data-access/services/category.service.ts
+++ app/codepulse/src/app/admin/data-access/services/category.service.ts
@@ -3,21 +3,24 @@ import { AddCategoryRequest } from '../models/add-category-request.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Category } from '../models/category.model';
+import { AppConfigService } from 'src/app/app-config.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CategoryService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private appConfig: AppConfigService) {}
 
   addCategory(addCategoryRequest: AddCategoryRequest): Observable<void> {
     return this.http.post<void>(
-      `https://localhost:44327/api/categories`,
+      `${this.appConfig.config?.apibaseURL}/api/categories`,
       addCategoryRequest
     );
   }
 
   getAllCategories(): Observable<Category[]> {
-    return this.http.get<Category[]>(`https://localhost:44327/api/categories`);
+    return this.http.get<Category[]>(
+      `${this.appConfig.config?.apibaseURL}/api/categories`
+    );
   }
 }
