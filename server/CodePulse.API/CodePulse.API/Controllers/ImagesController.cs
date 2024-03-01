828cdf938a0208fff16f514844110d2ac2a66cea
Motheo Moiloa <motheo@bbd.co.za>
2024-03-01 13:53:37 +0200

    feat: image Controllers
    -Add BlogImage Domain model
    -Add Images Controllers

---
+++ server/CodePulse.API/CodePulse.API/Controllers/ImagesController.cs
@@ -0,0 +1,46 @@
+﻿using CodePulse.API.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        //POST: https://localhost:xxxx/api/images
+        [HttpPost]
+        public async Task<IActionResult> uploadImage([FromForm] IFormFile file, [FromForm] string fileName, [FromForm] string title)
+        {
+            ValidateFileUpload(file);
+
+            if (ModelState.IsValid)
+            {
+                var blogImage = new BlogImage
+                {
+                    FileExtension = Path.GetExtension(file.FileName).ToLower(),
+                    FileName = fileName,
+                    Title = title,
+                    DateCreated = DateTime.Now,
+                }
+            }
+        }
+
+        private void ValidateFileUpload(IFormFile file)
+        {
+            var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png" };
+            
+            if (allowedExtensions.Contains(Path.GetExtension(file.FileName).ToLower()))
+            {
+                ModelState.AddModelError("file", "Unsupported file format");
+            }
+
+            if(file.Length > 10485768)
+            {
+                ModelState.AddModelError("file", "File size cannot exceed 10MB");
+            }
+        }
+
+    }
+}
