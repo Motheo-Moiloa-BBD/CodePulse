afee67c0103d5d6389772abdea23537eccdbd6f7
Motheo Moiloa <motheo@bbd.co.za>
2024-03-04 14:06:14 +0200

    Update home component to display list of blogpost cards

--- app/codepulse/src/app/home/features/home/home.component.ts
+++ app/codepulse/src/app/home/features/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BlogPost } from 'src/app/admin/data-access/models/blog-post.model';
+import { BlogPostService } from 'src/app/admin/data-access/services/blog-post.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  blogs$?: Observable<BlogPost[]>;
 
+  constructor(private blogpostService: BlogPostService) {}
+
+  ngOnInit(): void {
+    this.blogs$ = this.blogpostService.getAllBlogPosts();
+  }
 }
