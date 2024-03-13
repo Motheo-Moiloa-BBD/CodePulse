a185902b3dfc529ca65f033429b16b9bae687494
Motheo Moiloa <motheo@bbd.co.za>
2024-03-13 15:34:03 +0200

    Add first DOM interaction spec for the category list component

--- app/codepulse/src/app/admin/features/category-list/category-list.component.spec.ts
+++ app/codepulse/src/app/admin/features/category-list/category-list.component.spec.ts
@@ -7,12 +7,15 @@ import { mockCategories } from 'src/app/mocking/mock-categories';
 import { of } from 'rxjs';
 import { mockCategoryService } from 'src/app/mocking/category-service-mock';
 import { RouterTestingModule } from '@angular/router/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('CategoryListComponent', () => {
   let component: CategoryListComponent;
   let fixture: ComponentFixture<CategoryListComponent>;
   let categoryService: CategoryService;
 
+  //used async since compileComponents() is asynchronous
   beforeEach(async () => {
     TestBed.configureTestingModule({
       declarations: [CategoryListComponent],
@@ -44,5 +47,13 @@ describe('CategoryListComponent', () => {
     });
   });
 
-  //DOM interactions testing
+  //DOM interactions : HTML template unit tests
+  it('should display category list heading on render', () => {
+    const headingDe: DebugElement = fixture.debugElement;
+    const h1De = headingDe.query(By.css('h1'));
+    const h1: HTMLElement = h1De.nativeElement;
+
+    //h1 element should render the heading
+    expect(h1.textContent).toEqual('Category List');
+  });
 });
