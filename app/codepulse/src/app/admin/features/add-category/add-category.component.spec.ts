1cd607fe7678e870411f30a455f85e9cb9e373ee
Motheo Moiloa <motheo@bbd.co.za>
2024-03-15 12:10:10 +0200

    Add category initial unit tests

--- app/codepulse/src/app/admin/features/add-category/add-category.component.spec.ts
+++ app/codepulse/src/app/admin/features/add-category/add-category.component.spec.ts
@@ -1,21 +1,68 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AddCategoryComponent } from './add-category.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CategoryService } from '../../data-access/services/category.service';
+import { MockCategoryService } from 'src/app/mocking/category-service-mock';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router, Routes } from '@angular/router';
+import { Location } from '@angular/common';
+import { CategoryListComponent } from '../category-list/category-list.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('AddCategoryComponent', () => {
   let component: AddCategoryComponent;
   let fixture: ComponentFixture<AddCategoryComponent>;
+  let categoryService: CategoryService;
+  let router: Router;
+  let location: Location;
+  let debugElement: DebugElement;
+
+  beforeEach(waitForAsync(() => {
+    const routes: Routes = [
+      {
+        path: 'admin/categories',
+        component: CategoryListComponent,
+      },
+    ];
+
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        ReactiveFormsModule,
+        RouterTestingModule.withRoutes(routes),
+      ],
+      declarations: [AddCategoryComponent],
+      providers: [{ provide: CategoryService, useClass: MockCategoryService }],
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [AddCategoryComponent]
-    });
     fixture = TestBed.createComponent(AddCategoryComponent);
     component = fixture.componentInstance;
+
+    categoryService = TestBed.inject(CategoryService);
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
+    debugElement = fixture.debugElement;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  //Class Testing
+
+  //DOM interactions : HTML template unit tests
+  it('should display category list heading on render', () => {
+    const h1De = debugElement.query(By.css('h1'));
+    const h1: HTMLElement = h1De.nativeElement;
+
+    //h1 element should render the heading
+    expect(h1.textContent).toEqual('Add Category');
   });
 });
