639462e33d35e239798e563c2c1a8581d260444c
Motheo Moiloa <motheo@bbd.co.za>
2024-03-19 16:32:10 +0200

    Add image selector unit tests

--- app/codepulse/src/app/mocking/image-service-mock.ts
+++ app/codepulse/src/app/mocking/image-service-mock.ts
@@ -6,4 +6,16 @@ export class MockImageService {
   onSelectImage(): Observable<BlogImage> {
     return of(mockImages[1]);
   }
+
+  getAllImages(): Observable<BlogImage[]> {
+    return of(mockImages);
+  }
+
+  uploadImage(
+    file: File,
+    fileName: string,
+    title: string
+  ): Observable<BlogImage> {
+    return of(mockImages[0]);
+  }
 }
--- app/codepulse/src/app/shared/features/image-selector/image-selector.component.spec.ts
+++ app/codepulse/src/app/shared/features/image-selector/image-selector.component.spec.ts
@@ -1,21 +1,65 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+  waitForAsync,
+} from '@angular/core/testing';
 
 import { ImageSelectorComponent } from './image-selector.component';
+import { ImageService } from '../../data-access/services/image.service';
+import { MockImageService } from 'src/app/mocking/image-service-mock';
+import { FormsModule, NgForm } from '@angular/forms';
+import { mockImages } from 'src/app/mocking/mock-images';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
 
 describe('ImageSelectorComponent', () => {
   let component: ImageSelectorComponent;
   let fixture: ComponentFixture<ImageSelectorComponent>;
+  let imageService: ImageService;
+  let debugElement: DebugElement;
+  let mockForm: NgForm;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [FormsModule],
+      declarations: [ImageSelectorComponent],
+      providers: [
+        { provide: ImageService, useClass: MockImageService },
+        {
+          provide: NgForm,
+          useValue: { resetForm: jasmine.createSpy('resetForm') },
+        },
+      ],
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ImageSelectorComponent]
-    });
     fixture = TestBed.createComponent(ImageSelectorComponent);
     component = fixture.componentInstance;
+
+    imageService = TestBed.inject(ImageService);
+    mockForm = TestBed.inject(NgForm);
+    debugElement = fixture.debugElement;
+
+    //ngOnit()
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  xit('should upload image', () => {
+    const mockFile = new File([''], 'test.jpg', { type: 'image/jpeg' });
+    component.fileName = 'test-file';
+    component.title = 'test-title';
+    component['file'] = mockFile;
+
+    component.uploadImage();
+
+    expect(mockForm.resetForm).toHaveBeenCalled();
   });
 });
